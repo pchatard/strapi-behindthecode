2372b006da4850b097a81a59513f68d2d802f431
pchatard <chatard.pierre@gmail.com>
2020-11-23 10:59:50 +0100

    Remove provider

--- extensions/upload/config/settings.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  provider: "cloudinary",
-  providerOptions: {
-    cloud_name: env("deo7szuol"),
-    api_key: env("786912121686347"),
-    api_secret: env("MgTBsjSkPhvO9hTHNjDBSJszHmw"),
-  },
-};
