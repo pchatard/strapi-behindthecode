fdbd5014f60ccd7a2d25a868d797b60ff77c6a01
pchatard <chatard.pierre@gmail.com>
2020-11-23 14:40:40 +0100

    Cloudinary setup

---
+++ extensions/upload/config/settings.js
@@ -0,0 +1,8 @@
+module.exports = ({ env }) => ({
+  provider: "cloudinary",
+  providerOptions: {
+    cloud_name: env("deo7szuol"),
+    api_key: env("786912121686347"),
+    api_secret: env("MgTBsjSkPhvO9hTHNjDBSJszHmw"),
+  },
+});
